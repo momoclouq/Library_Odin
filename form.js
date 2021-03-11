f115d8a47beec4c366f91015742f2ea14ad399db
momocloud <phamhoangminh050899@gmail.com>
2021-03-11 11:17:12 +0700

    add input checking functions

--- form.js
+++ form.js
@@ -6,41 +6,61 @@ let submitBookBtn = document.querySelector("#submitBookBtn");
 //show the form when the button pressed
 showFormButton.addEventListener("click", () => {
     form.classList.toggle("isVisible");
+    resetForm();
 });
 
 closeFormButton.addEventListener("click", () => {
     form.classList.remove("isVisible");
+    resetForm();
 });
+
+function resetForm(){
+    //initialize all the required field for input
+    let titleField = document.querySelector("#bTitle");
+    let authorField = document.querySelector("#bAuthor");
+    let pagesField = document.querySelector("#bPages");
+
+    authorField.value = "";
+    pagesField.value = "";
+    titleField.value = "";
+}
 
 //get the data from the form and create a book
 submitBookBtn.addEventListener('click', function(){
+    //initialize all the required field for input
     let titleField = document.querySelector("#bTitle");
-    let title = titleField.value;
-
     let authorField = document.querySelector("#bAuthor");
-    let author = authorField.value;
-
     let pagesField = document.querySelector("#bPages");
+    let readField = document.getElementsByName("readValue");
+
+    //get the value
+    let title = titleField.value;
+    let author = authorField.value;
     let pages = pagesField.value;
 
-    let readField = document.getElementsByName("readValue");
-    console.log(readField);
-    let read = true;
+    let test = title && author && pages && 0;
 
-    for (let i = 0; i < readField.length; i++){
-        if (readField[i].checked){
-            read = (readField[i].value === "yes");
-            break;
+    if (test !== 0) alert("data missing, please enter all the data");
+    else if (isNaN(pages)) alert("pages input is not a number baka");
+    else {
+        let read = true;
+
+        for (let i = 0; i < readField.length; i++){
+            if (readField[i].checked){
+                read = (readField[i].value === "yes");
+                readField[i].checked = false;
+                break;
+            }
         }
-    }
 
-    let book = new Book(title, author, pages, read);
-    myLibrary.push(book);
-    let card = book.createCard(myLibrary.length-1);
-    container.appendChild(card);
-    authorField.value = "";
-    pagesField.value = "";
-    titleField.value = ""; 
+        let book = new Book(title, author, pages, read);
+        myLibrary.push(book);
+        let card = book.createCard(myLibrary.length-1);
+        container.appendChild(card);
+        authorField.value = "";
+        pagesField.value = "";
+        titleField.value = "";
+    }
 });
 
 
